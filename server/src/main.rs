2edc547448e7e5bb7816d29e721f1b2bd8767a0a
Ganesha Danu Enastika <ganeshadanu1@gmail.com>
2024-01-13 23:42:27 +0700

    feat: implement server port

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,16 @@
 use actix_files::NamedFile;
 use actix_web::{get, web, App, HttpServer, Result};
 
+#[get("/")]
+async fn index() -> Result<NamedFile> {
+    let filename = "index.html";
+    let path = format!("dist/{}", filename);
+
+    Ok(NamedFile::open(path)?)
+}
+
 #[get("/{filename:.*}")]
-async fn index(params: web::Path<String>) -> Result<NamedFile> {
+async fn dist(params: web::Path<String>) -> Result<NamedFile> {
     let filename = params.into_inner();
     let path = format!("dist/{}", filename);
 
@@ -11,10 +19,14 @@ async fn index(params: web::Path<String>) -> Result<NamedFile> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let server = HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
+    let port = match std::env::var_os("PORT") {
+        Some(v) => v.to_str().unwrap().parse().unwrap(),
+        None => 8080,
+    };
+    let server = HttpServer::new(|| App::new().service(index).service(dist))
+        .bind(("127.0.0.1", port))?
         .run();
 
-    println!("server running on :8090");
+    println!("server running on {}", port);
     server.await
 }
